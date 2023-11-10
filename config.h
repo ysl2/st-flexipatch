b6805990c93dfd7d3a12329936c027cdcaff6741
ysl2 <www.songli.yu@gmail.com>
2023-11-10 09:04:51 +0800

    Add scroll support.
    Change ksroll keys

--- config.h
+++ config.h
@@ -454,8 +454,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Return,      fullscreen,      {.i =  0} },
 	#endif // FULLSCREEN_PATCH
 	#if SCROLLBACK_PATCH
-	{ ShiftMask,            XK_Page_Up,     kscrollup,       {.i = -1}, S_PRI },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,     {.i = -1}, S_PRI },
+	{ MODKEY,               XK_k,           kscrollup,       {.i = -1}, S_PRI },
+	{ MODKEY,               XK_j,           kscrolldown,     {.i = -1}, S_PRI },
 	#endif // SCROLLBACK_PATCH
 	#if CLIPBOARD_PATCH
 	{ TERMMOD,              XK_Y,           clippaste,       {.i =  0} },
